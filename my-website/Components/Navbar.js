8699657bac1e8655bf160244ee7ef3e99d077185
iTakedown27 <ezrabridger27@gmail.com>
2024-01-16 14:20:46 -0800

    first commit

---
+++ my-website/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+        <a>Home</a>
+        <a>Experience</a>
+        <a>Portfolio</a>
+        <a>Journey</a>
+    </div>
+  );
+}
+
+export default Navbar;
