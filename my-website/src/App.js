dc1e23e4e3ae213fd0f4fa16deb9a489ec6b1de6
iTakedown27 <ezrabridger27@gmail.com>
2024-01-16 18:22:47 -0800

    Make Home Component

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, th
-        </p>
+        <meta name='viewport'></meta>
       </header>
+      <Home />
     </div>
   );
 }
